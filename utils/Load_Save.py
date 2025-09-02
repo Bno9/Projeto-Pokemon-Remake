7d6254f3d6a52df892a19772bbe907e955a1daae
Bno <Breno_live2005@hotmail.com>
2025-09-01 22:35:29 -0300

    Reestruturação do Json e finalização dos requests

--- utils/Load_Save.py
+++ utils/Load_Save.py
@@ -4,32 +4,41 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from classes.Pokemons import Pokemon
-from classes.Itens import Item
-from utils.api import get_pokeapi
+from classes.Itens import Item 
+from utils.api import get_pokeapi, get_all_generation
 
-lista_pokemons = []
-lista_itens = []
+def carregar_json(file, classe, campos):
 
+    lista = []
+    try:
+        with open(file, "r", encoding="utf-8") as arquivo:
+            dados = json.load(arquivo)
 
-try:
-    with open("pokemons.json", "r", encoding="utf-8") as arquivo:
-        dados = json.load(arquivo)
+            for item, valor in dados.items():
+                args = [item] + [valor[campo] for campo in campos]
+                lista.append(classe(*args))
+    except (json.JSONDecodeError, FileNotFoundError):
+        pass
+    return lista
 
-        for item, valor in dados.items():
-            Poke_formatado = Pokemon(item, valor["tipos"], valor["stats"])
-            lista_pokemons.append(Poke_formatado)
-except json.JSONDecodeError:
-    lista_pokemon = []
-
-
-
+lista_pokemons = carregar_json("pokemons.json", Pokemon, ["tipos", "stats"])
+lista_itens = carregar_json("itens.json", Item, ["custo", "categoria", "atributos"])
 
 
 def pedir_input(endpoint):
     return input(f"Digite o nome do {endpoint}: ")
  
-
 def criar_pokemon(endpoint):
+    """
+    Função que cria um pokemon atraves do nome ou id passado pelo usuario
+    
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
     nome = pedir_input(endpoint)
 
     for i in lista_pokemons:
@@ -50,7 +59,45 @@ def criar_pokemon(endpoint):
     Poke = Pokemon(pokemon_json["name"], tipos, stats)
     lista_pokemons.append(Poke)
 
+    salvar_json(lista_pokemons, "pokemons.json", poke_dict)
+
+def criar_geracao():  #apenas um teste porque ainda nao tenho certeza de como quero fazer a criação dos pokemons, mas por enquanto vou trabalhar apenas com a primeira geração
+    """
+    Função que cria todos os pokemons da primeira geração (pode ser alterado para qualquer geração,
+    basta ir na api.py e mudar o endpoint do get_all_generation)
+    e guarda em uma lista (lista_pokemons)
+    
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    geracao1 = get_all_generation()
+
+
+    for i in geracao1["pokemon_species"]:
+        pokemon_json = get_pokeapi(i["name"], "pokemon")
+
+        stats = {stat['stat']['name']: stat['base_stat'] for stat in pokemon_json["stats"]}
+        tipos = [tipo['type']['name'] for tipo in pokemon_json['types']]
+
+        Poke = Pokemon(pokemon_json["name"], tipos, stats)
+        lista_pokemons.append(Poke)
+
+        salvar_json(lista_pokemons, "pokemons.json", poke_dict)
+
 def criar_item(endpoint):
+    """
+    Função que cria um item atraves do nome ou id passado pelo usuario
+    
+    Args:
+        None
+
+    Returns:
+        dict: Json da pokeapiNone
+    """
+
     nome = pedir_input(endpoint)
 
     item_json = get_pokeapi(nome, endpoint)
@@ -63,19 +110,27 @@ def criar_item(endpoint):
     itens = Item(item_json["name"], item_json["cost"], item_json["category"]["name"], [item["name"] for item in item_json["attributes"]])
     lista_itens.append(itens)
 
+    salvar_json(lista_itens, "itens.json", item_dict)
+
 def criacao():
+    """
+    Função que recebe o endpoint escolhido pelo usuario,
+    verifica se existe a opção, assimila a chave do dicionario com o valor,
+    e envia o endpoint para função respectiva
+    
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
     end_map = {"pokemon": criar_pokemon,
            "item": criar_item}
     
 
     while True:
-        for i in lista_itens:
-            print(i.nome)
-
-        for i in lista_pokemons:
-            print(i.nome)
-
-        endpoint = input("Digite qual requisição deseja fazer (pokemon, item. 0 pra sair):  ")
+        endpoint = input("Digite qual endpoint deseja acessar (pokemon, item. 0 pra sair):  ")
 
         if endpoint == "0":
             return
@@ -84,15 +139,23 @@ def criacao():
 
         escolha(endpoint)
 
+def poke_dict(obj):
+    return{
+        "tipos": obj.tipos,
+        "stats": obj.stats
+    }
+
+def item_dict(obj):
+    return{
+        "custo": obj.custo,
+        "categoria": obj.categoria,
+        "atributos": obj.atributos
+    }
+
+def salvar_json(lista, file, mapa_conversao):
+    with open(file, "w", encoding="utf-8") as arquivo:
+        dados = {obj.nome: mapa_conversao(obj) for obj in lista}
+        
+        json.dump(dados, arquivo, indent=4)    
 
 criacao()
-
-with open("pokemons.json", "w", encoding="utf-8") as arquivo:
-    json_formatado = {}
-
-    for poke in lista_pokemons:
-        json_formatado[poke.nome] = {"tipos": poke.tipos,
-                                    "stats": poke.stats}
-     
-    json.dump(json_formatado, arquivo, indent=4)
-
--- utils/api.py
+++ utils/api.py
@@ -1,9 +1,20 @@
 import requests
+from datetime import datetime, time
 
 BASE_URL = "https://pokeapi.co/api/v2"
 
 
 def get_pokeapi(nome_ou_id, endpoint):
+    """
+    Função que retorna um pokemon, item ou ataque, dependendo do endpoint passado
+    
+    Args:
+        nome_ou_id (str|int): Nome ou id que deseja buscar da api 
+        endpoint (str): Endpoint da api que vai ser acessada
+
+    Returns:
+        dict: Json da pokeapi
+    """
 
     url = f"{BASE_URL}/{endpoint}/{nome_ou_id}"
     response = requests.get(url)
@@ -11,6 +22,37 @@ def get_pokeapi(nome_ou_id, endpoint):
     if response.status_code != 200:
         raise ValueError(f"{endpoint} {nome_ou_id} não encontrado.")
     
-    print(f"{endpoint} transferido da api com sucesso")
+    # print(f"{endpoint} transferido da api com sucesso")
 
+    return response.json()
+
+
+def get_all_generation():
+    """
+    Função que retorna toda a geração 1 de pokemons da pokeapi
+    (pode retornar qualquer outra, basta mudar o final do endpoint)
+    
+    Args:
+        None
+
+    Returns:
+        dict: Json da pokeapi
+    """
+
+    url = f"{BASE_URL}/generation/1"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise ValueError(f"geração não encontrada.")
+    
+    teste = response.json()
+
+    inicio = datetime.now()
+
+    for j in teste["pokemon_species"]:
+        final = datetime.now()
+        tempo = final - inicio
+        print(f"{j["name"]} transferido da api com sucesso às {datetime.now()} Tempo de espera: {tempo}")
+
+    print("Geração transferida com sucesso.")
     return response.json()
