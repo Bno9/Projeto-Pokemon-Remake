4db740fa075e75410ce6fbab1b460117513c53ad
Bno <95531780+Bno9@users.noreply.github.com>
2025-09-26 12:21:54 -0300

    Corrigindo o que faltou no último commit

--- classes/Pokemons.py
+++ classes/Pokemons.py
@@ -27,7 +27,7 @@ class Pokemon:
             "tipos": self.tipos,
                 "stats": self.stats,
                 "hp_atual": self.hp_atual,
-                "can_learn": [ataque.to_dict() for ataque in self.can_learn]
+                "can_learn": [{ataque["ataque"].to_dict() for ataque in self.can_learn, "level_earned_at": ataque["level_earned_at"]}]
                 }
 
 
